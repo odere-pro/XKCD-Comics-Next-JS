141973bdb35a38be4c287d7e3465916812c512f1
odere-pro <165695298+odere-pro@users.noreply.github.com>
2024-06-10 00:41:30 +0200

    feat: display batches of comicses on home page, add BreadCrumbs to root layout, optimize for different devices

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,120 @@
-import Image from 'next/image'
+import Comics from '@/components/Comics'
+import { ArrowLongUpIcon, ArrowLongDownIcon } from '@heroicons/react/20/solid'
+import { fetchComicsByIds, getLastComicsID } from '@/lib/api'
+import { getBatchIds } from '@/lib/utils'
+import { notFound } from 'next/navigation'
 
-export default function Home() {
+const ONE_BATCH = 12
+
+interface HomeProps {
+    params: {}
+    searchParams: { start: string; end: string; batch: string }
+}
+
+const parseSearchParams = (
+    searchParams: HomeProps['searchParams'],
+    lastID = 1,
+): {
+    batch: number
+    end: number
+    start: number
+} => {
+    let batch = ONE_BATCH
+    let end = lastID
+    let start
+
+    if (searchParams.batch) {
+        try {
+            batch = parseInt(searchParams.batch, 10)
+        } catch (error) {
+            console.warn('Failed to parse "batch" searchParams')
+        }
+    }
+
+    batch = batch > ONE_BATCH ? ONE_BATCH : batch
+
+    if (searchParams.end) {
+        try {
+            end = parseInt(searchParams.end, 10)
+        } catch (error) {
+            console.warn('Failed to parse "batch" searchParams')
+        }
+    }
+
+    end = end - ONE_BATCH <= 0 ? ONE_BATCH : end
+    start = end - batch + 1
+
+    if (searchParams.start) {
+        try {
+            start = parseInt(searchParams.start, 10)
+        } catch (error) {
+            console.warn('Failed to parse "batch" searchParams')
+        }
+    }
+
+    // Make sure that window is no more then batch
+    if (start + batch < end) {
+        end = start + batch
+    }
+
+    return {
+        batch,
+        end,
+        start,
+    }
+}
+// Going with sliding window size of no more then 24 images to tackle performance, and internat traffic issues
+export default async function Home(props: HomeProps) {
+    const { searchParams } = props
+    const lastID = await getLastComicsID()
+    const { start, end, batch } = parseSearchParams(searchParams, lastID)
+    const nextStart = start - batch
+    const nextEnd = end - batch
+    const prevStart = start + batch
+    const prevEnd = end + batch
+
+    const ids = getBatchIds(start, end)
+    let data = await fetchComicsByIds(ids)
+
+    if (!data) {
+        notFound()
+    }
+
+    data = data.reverse()
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
-                    Get started by editing&nbsp;
-                    <code className="font-mono font-bold">app/page.tsx</code>
-                </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
-                    <a
-                        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By{' '}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className="dark:invert"
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
-                </div>
-            </div>
-
-            <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40">
-                <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-            <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Docs{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                        Find in-depth information about Next.js features and API.
-                    </p>
+        <>
+            {/* Goto page with prev batch of comics */}
+            {prevStart < lastID && (
+                <a className="mt-8 inline-flex" href={`/?start=${prevStart}&end=${prevEnd}`}>
+                    <ArrowLongUpIcon className="h-5 w-5 text-gray-400 md:mr-3" aria-hidden="true" />
+                    <span>Show previous</span>
                 </a>
+            )}
 
+            <ul
+                role="list"
+                className="image-grid mt-8 grid w-full grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-2 lg:grid-cols-3"
+            >
+                {data.map((file, idx) => (
+                    <li key={idx}>
+                        <Comics {...file} alt={file.alt} withImageLink />
+                    </li>
+                ))}
+            </ul>
+            {/* Goto page with next batch of comics */}
+            {nextEnd > 1 && (
                 <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                    className="mt-8 inline-flex"
+                    href={`/?start=${nextStart}&end=${nextEnd}&batch=${batch}`}
                 >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Learn{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                        Learn about Next.js in an interactive course with&nbsp;quizzes!
-                    </p>
+                    <ArrowLongDownIcon
+                        className="h-5 w-5 text-gray-400 md:mr-3"
+                        aria-hidden="true"
+                    />
+                    <span>Show more</span>
                 </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Templates{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                        Explore starter templates for Next.js.
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Deploy{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-                        Instantly deploy your Next.js site to a shareable URL with Vercel.
-                    </p>
-                </a>
-            </div>
-        </main>
+            )}
+        </>
     )
 }
