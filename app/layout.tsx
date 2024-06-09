67ccf917cc3041c3fefcc1ccb23665a05864ca1d
odere-pro <165695298+odere-pro@users.noreply.github.com>
2024-06-10 01:23:14 +0200

    fix: Breadcrumbs styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: 'XKCD Webcomic',
-    description: 'This is a simple UI to explore XKCD strips - https://xkcd.com. It is built using Next.js and TypeScript.',
+    description:
+        'This is a simple UI to explore XKCD strips - https://xkcd.com. It is built using Next.js and TypeScript.',
 }
 
 type RootLayoutProps = Readonly<{
@@ -21,9 +22,16 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <html lang="en">
             <body className={clsx(inter.className, 'flex min-h-screen flex-col items-center')}>
                 <header className="content-bg w-full max-w-5xl px-4 md:px-8 lg:px-16">
-                    <div className="flex items-center justify-between border-b border-gray-400 py-4">
+                    <div className="flex flex-col-reverse items-start justify-between gap-2 border-b border-gray-400 py-4 text-sm md:flex md:flex-row md:items-center md:gap-0 md:text-base">
                         <BreadCrumbs />
-                        <h1>XKCD - A webcomic of romance, sarcasm, math, and language.</h1>
+                        <h1>
+                            XKCD - A webcomic
+                            <span className="hidden md:inline">
+                                {' '}
+                                of romance, sarcasm, math, and language
+                            </span>
+                            .
+                        </h1>
                     </div>
                 </header>
                 <main className="content-bg flex h-fit w-full max-w-5xl grow flex-col items-center px-4 pb-4 md:px-8 md:pb-8 lg:px-16 lg:pb-16">
--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -40,27 +40,27 @@ const Breadcrumbs = () => {
     }, [fetchComicsById, id])
 
     return (
-        <nav className="flex" aria-label="Breadcrumb">
-            <ol role="list" className="flex items-center space-x-4">
-                <li>
+        <nav className="breadcrumbs" aria-label="Breadcrumb">
+            <ol role="list" className="flex justify-end space-x-4">
+                <li className="m-0">
                     <div>
-                        <a href="/" className="text-gray-400 hover:text-gray-500">
-                            <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
-                            <span className="sr-only">Home</span>
+                        <a href="/" className="flex gap-1 text-gray-400 hover:text-gray-500">
+                            <HomeIcon className="h-5 flex-shrink-0" aria-hidden="true" />
+                            <span>Home</span>
                         </a>
                     </div>
                 </li>
 
                 {data?.title && (
-                    <li>
+                    <li className="m-0">
                         <div className="flex items-center">
                             <ChevronRightIcon
-                                className="h-5 w-5 flex-shrink-0 text-gray-400"
+                                className="h-5 flex-shrink-0 text-gray-400"
                                 aria-hidden="true"
                             />
                             <a
                                 href={`/comics/${data?.num}`}
-                                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                                className="text-sm font-medium text-gray-500 hover:text-gray-700"
                                 aria-current={'page'}
                             >
                                 {data.title}
